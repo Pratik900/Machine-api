52e8ed31e6b3f92f707fbab9b795f4bfa77b2810
Pratik900 <pratikgurav900@gmail.com>
2023-12-11 09:33:52 +0530

    Login token api Done

--- Index.js
+++ Index.js
@@ -2,10 +2,35 @@ import express, { response } from 'express';
 import mongoose from 'mongoose'
 import { MachineDetails } from './Dbfile.js';
 import cors from 'cors'
+import { Admin } from './AdminModel.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+function verifyToken(request,response,next){
+    const header = request.get('Authorization');
+    if(header)
+    {
+        const token = header.split(" ")[1];
+        jwt.verify(token,"secret1234",(error,payload)=>{
+            if(error){
+                response.send({message:"Invalid token"});
+            }
+            else
+            {
+                next();
+            }
+        });
+    }else{
+        response.send({message:"Please login first!"})
+    }
+    
+    }
+
+
 
 const app=express();
-app.use(cors())
 
+app.use(cors())
 app.use(express.json());
 
 const connectDb = async()=>{
@@ -16,14 +41,43 @@ const connectDb = async()=>{
         console.log("Error db not connected");
     }
 }
+app.post("/admin",async(request,response)=>{
+    try{
+       const reqData = request.body;
+       reqData['password'] = bcrypt.hashSync(reqData.password,10);
+       const admin = new Admin(reqData);
+        await admin.save();
+        response.send({message :"Data Inserted"});
+    }
+    catch(error){
+      response.send({message: "Oops! Something went wrong..."});
+    }
+});
 
-// app.get("/hello",async(request,response)=>{
-//     try{
-//         response.send("Hello Pratik");
-//     } catch(error){
-//         response.send(error)
-//     }
-// })
+app.post("/admin/login",async(request,response)=>{
+    try{
+        
+      const admin = await Admin.findOne({phone:request.body.phone});
+      if(admin){
+        if(bcrypt.compareSync(request.body.password,admin.password))
+        {
+            const token=jwt.sign({adminPhone:admin.phone},"secret1234");
+            response.send({message: "Login successful",token:token});
+        }
+        else{
+            response.send({message: "Invalid Password"});
+        }
+      }
+      else
+      {
+        response.send({message: "Invalid Phone"});
+      }
+
+    }
+    catch(error){
+      response.send({message: "Oops! Inavlid phone or password..."});
+    }
+});
 
 app.post("/machine",async(request,response)=>{
     try{
@@ -97,6 +151,50 @@ app.get("/countno/:working",async(request,response)=>{
         response.send('Something not found..!')
     }
 })
+
+app.get("/emergencycountyes/:emergenyrepair",async(request,response)=>{
+    try{
+        const count = await MachineDetails.countDocuments({
+            emergenyrepair: { $in: ['yes', 'Yes'] }
+        });
+            response.json({ count });
+    }catch(error){
+        response.send('Something not found..!')
+    }
+})
+app.get("/emergencycountno/:emergenyrepair",async(request,response)=>{
+    try{
+        const count = await MachineDetails.countDocuments({
+            emergenyrepair: { $in: ['no', 'No'] }
+        });
+            response.json({ count });
+    }catch(error){
+        response.send('Something not found..!')
+    }
+})
+
+
+app.get("/rates", (_req, res, _next) => {
+    res.set({
+      "Access-Control-Allow-Origin": "*",
+      "Cache-Control": "no-cache",
+      Connection: "keep-alive", // allowing TCP connection to remain open for multiple HTTP requests/responses
+      "Content-Type": "text/event-stream", // media type for Server Sent Events (SSE)
+    });
+    res.flushHeaders();
+  
+    const interval = setInterval(() => {
+      const stock1Rate = Math.floor(Math.random() * 100000);
+      const stock2Rate = Math.floor(Math.random() * 60000);
+      res.write(`data: ${JSON.stringify({ stock1Rate, stock2Rate })}\n\n`);
+    }, 2000);
+  
+    res.on("close", () => {
+      clearInterval(interval);
+      res.end();
+    });
+  });
+
 
 app.listen(4900,()=>{
     console.log('Server is running on port 4900');
